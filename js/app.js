6647bf568a6e1d69ce87a817b9e1a9b71ff0e2e4
acox1993 <alexandriacrmu@gmail.com>
2016-05-23 07:45:14 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	var contactsarray = [];
+
+	var Contact = {
+		contactID: "",
+		firstName: "",
+		lastName: "",
+		phoneNumber: "",
+		Street: "",
+		City: "",
+		State: "",
+	};
+
+	contactsarray.push(object.create(Contact));
+
+	// contactsarray.sort().appendto("div.contacts")
+	$("div.contacts").append();
+});
+
+// 1. Build out non-interactive version no css
+// 2. commit
+// 3. style
+// 4. commit
+// 5. make responsive
+// 6. commit
+// 7. have information post to plain array 
+// 8. commit
+// 9. make sure array sorts before being posted
+// 10. commit
+// 11. create prototype object
+// 12. commit
+// 13. have information post in new sub of original prototype
+// 14. commit
+// 15. have objects push to array
+// 16. commit
+// 17. when selected it pops up in screen to show object information
+// 18. commit
+// 19. all interactive
+// 20. commit
+// 21. go on to version 2
