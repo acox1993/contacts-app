261a6c710709c386a9369e2aa5de2559306af600
acox1993 <alexandriacrmu@gmail.com>
2016-06-06 10:43:30 -0400

    finctionality

--- js/app.js
+++ js/app.js
@@ -1,20 +1,47 @@
 $(document).ready(function(){
-	var contactsarray = [];
+	contactID = 0;
+	contactsArray = [];
 
-	var Contact = {
-		contactID: "",
-		firstName: "",
-		lastName: "",
-		phoneNumber: "",
-		Street: "",
-		City: "",
-		State: "",
-	};
+	$('.add').click(function(){
+		var firstName = $('.firstName').val();
+		var lastName = $('.lastName').val();
+		var phoneNumber = $('.phoneNumber').val();
+		var street = $('.street').val();
+		var city = $('.city').val();
+		var state = $('.state').val();
+		$('input.firstName').val("");
+		$('input.lastName').val("");
+		$('input.phoneNumber').val("");
+		$('input.street').val("");
+		$('input.city').val("");
+		$('input.state').val("");
 
-	contactsarray.push(object.create(Contact));
+		
 
-	// contactsarray.sort().appendto("div.contacts")
-	$("div.contacts").append();
+		contactsArray.push({'contactID': contactID,'firstName': firstName, 'lastName': lastName, 'phoneNumber': phoneNumber, 'street': street, 'city': city, 'state': state});
+		contactID++;
+
+	});
+		$('button.update').click(function(){
+			$('div.contacts ul.con').html("");
+			$.each(contactsArray, function() { 
+				console.log(this);
+				var contactHTML = "<div class='contactwrapper' id="+ this.contactID +"> <h4>" + this.firstName + " " + this.lastName + "</h4> <ul class="+ this.contactID+"> <li>First name: "+ this.firstName + "</li><li>Last name: " + this.lastName + " </li><li>Phone number: "+ this.phoneNumber +"</li><li>Address: "+ this.street +", "+ this.city +"	, "+ this.state +"</li></ul> </div>";
+			$("div.contacts").append(contactHTML);
+			var contactLi = "<li> <a class="+ this.contactID+"> "+ this.firstName +" "+ this.lastName +"</a></li>";
+			$(".contacts ul.con").append(contactLi);
+			});
+			$("li a").click(function(){
+				$('div.appear').html("");
+				var id = $(this).attr("class");
+				$('div#' + id).show().appendTo('.appear');
+
+			});
+
+
+		});
+
+	
 });
 
 // 1. Build out non-interactive version no css
